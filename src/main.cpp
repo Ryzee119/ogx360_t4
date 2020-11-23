4176acd73835f3fd4d781d831566382ac610dc42
Ryzee119 <wendland@live.com.au>
2020-11-24 08:05:41 +1030

    XID: Refactor driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 #include "USBHost_t36.h"
 #include "tusb.h"
-#include "xid.h"
 #include "printf.h"
+
+#include "xid_duke.h"
 
 //USB Device Interface
 USB_XboxGamepad_InReport_t xpad_data;
@@ -20,32 +21,34 @@ void _putchar(char character)
 
 void usbd_isr(void)
 {
-    digitalToggle(13);
     tud_int_handler(0);
 }
 
 void setup()
 {
     Serial1.begin(115200);
+    printf("ogx360_t4 Starting!\r\n");
 
     memset(&xpad_data, 0x00, sizeof(xpad_data));
     xpad_data.bLength = sizeof(xpad_data);
 
     //Set onboard LED to output
-    pinMode(13, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
     //USB Device Interface Init
     NVIC_DISABLE_IRQ(IRQ_USB1);
     USB1_USBCMD |= USB_USBCMD_RST;
-    while (USB1_USBCMD & USB_USBCMD_RST)
-        ;
+    while (USB1_USBCMD & USB_USBCMD_RST);
     NVIC_CLEAR_PENDING(IRQ_USB1);
+    delay(10);
     attachInterruptVector(IRQ_USB1, &usbd_isr);
     NVIC_ENABLE_IRQ(IRQ_USB1);
     tusb_init();
+    printf("USB Device Stack Initialised\r\n");
 
     //USB Host Interface Init
     usbh.begin();
+    printf("USB Host Stack Initialised\r\n");
 }
 
 void loop()
@@ -70,57 +73,85 @@ void loop()
         {
         case JoystickController::XBOX360:
         case JoystickController::XBOX360_WIRED:
-            if (_buttons & (1 << 0)) xpad_data.dButtons |= XID_DUP;
-            if (_buttons & (1 << 1)) xpad_data.dButtons |= XID_DDOWN;
-            if (_buttons & (1 << 2)) xpad_data.dButtons |= XID_DLEFT;
-            if (_buttons & (1 << 3)) xpad_data.dButtons |= XID_DRIGHT;
-            if (_buttons & (1 << 4)) xpad_data.dButtons |= XID_START;
-            if (_buttons & (1 << 5)) xpad_data.dButtons |= XID_BACK;
-            if (_buttons & (1 << 6)) xpad_data.dButtons |= XID_LS;
-            if (_buttons & (1 << 7)) xpad_data.dButtons |= XID_RS;
+            if (_buttons & (1 << 0))
+                xpad_data.dButtons |= XID_DUP;
+            if (_buttons & (1 << 1))
+                xpad_data.dButtons |= XID_DDOWN;
+            if (_buttons & (1 << 2))
+                xpad_data.dButtons |= XID_DLEFT;
+            if (_buttons & (1 << 3))
+                xpad_data.dButtons |= XID_DRIGHT;
+            if (_buttons & (1 << 4))
+                xpad_data.dButtons |= XID_START;
+            if (_buttons & (1 << 5))
+                xpad_data.dButtons |= XID_BACK;
+            if (_buttons & (1 << 6))
+                xpad_data.dButtons |= XID_LS;
+            if (_buttons & (1 << 7))
+                xpad_data.dButtons |= XID_RS;
 
             //Analog buttons are converted to digital
-            if (_buttons & (1 << 8))  xpad_data.WHITE = 0xFF;
-            if (_buttons & (1 << 9))  xpad_data.BLACK = 0xFF;
-            if (_buttons & (1 << 12)) xpad_data.A = 0xFF;
-            if (_buttons & (1 << 13)) xpad_data.B = 0xFF;
-            if (_buttons & (1 << 14)) xpad_data.X = 0xFF;
-            if (_buttons & (1 << 15)) xpad_data.Y = 0xFF;
+            if (_buttons & (1 << 8))
+                xpad_data.WHITE = 0xFF;
+            if (_buttons & (1 << 9))
+                xpad_data.BLACK = 0xFF;
+            if (_buttons & (1 << 12))
+                xpad_data.A = 0xFF;
+            if (_buttons & (1 << 13))
+                xpad_data.B = 0xFF;
+            if (_buttons & (1 << 14))
+                xpad_data.X = 0xFF;
+            if (_buttons & (1 << 15))
+                xpad_data.Y = 0xFF;
 
             //Analog Sticks
-            xpad_data.leftStickX  = _axis[0];
-            xpad_data.leftStickY  = _axis[1];
+            xpad_data.leftStickX = _axis[0];
+            xpad_data.leftStickY = _axis[1];
             xpad_data.rightStickX = _axis[2];
             xpad_data.rightStickY = _axis[3];
-            xpad_data.L           = _axis[4];
-            xpad_data.R           = _axis[5];
+            xpad_data.L = _axis[4];
+            xpad_data.R = _axis[5];
 
             break;
         case JoystickController::XBOXONE:
-            if (_buttons & (1 << 8))  xpad_data.dButtons |= XID_DUP;
-            if (_buttons & (1 << 9))  xpad_data.dButtons |= XID_DDOWN;
-            if (_buttons & (1 << 10)) xpad_data.dButtons |= XID_DLEFT;
-            if (_buttons & (1 << 11)) xpad_data.dButtons |= XID_DRIGHT;
-            if (_buttons & (1 << 2))  xpad_data.dButtons |= XID_START;
-            if (_buttons & (1 << 3))  xpad_data.dButtons |= XID_BACK;
-            if (_buttons & (1 << 14)) xpad_data.dButtons |= XID_LS;
-            if (_buttons & (1 << 15)) xpad_data.dButtons |= XID_RS;
+            if (_buttons & (1 << 8))
+                xpad_data.dButtons |= XID_DUP;
+            if (_buttons & (1 << 9))
+                xpad_data.dButtons |= XID_DDOWN;
+            if (_buttons & (1 << 10))
+                xpad_data.dButtons |= XID_DLEFT;
+            if (_buttons & (1 << 11))
+                xpad_data.dButtons |= XID_DRIGHT;
+            if (_buttons & (1 << 2))
+                xpad_data.dButtons |= XID_START;
+            if (_buttons & (1 << 3))
+                xpad_data.dButtons |= XID_BACK;
+            if (_buttons & (1 << 14))
+                xpad_data.dButtons |= XID_LS;
+            if (_buttons & (1 << 15))
+                xpad_data.dButtons |= XID_RS;
 
             //Analog buttons are converted to digital
-            if (_buttons & (1 << 12)) xpad_data.WHITE = 0xFF;
-            if (_buttons & (1 << 13)) xpad_data.BLACK = 0xFF;
-            if (_buttons & (1 << 4))  xpad_data.A = 0xFF;
-            if (_buttons & (1 << 5))  xpad_data.B = 0xFF;
-            if (_buttons & (1 << 6))  xpad_data.X = 0xFF;
-            if (_buttons & (1 << 7))  xpad_data.Y = 0xFF;
+            if (_buttons & (1 << 12))
+                xpad_data.WHITE = 0xFF;
+            if (_buttons & (1 << 13))
+                xpad_data.BLACK = 0xFF;
+            if (_buttons & (1 << 4))
+                xpad_data.A = 0xFF;
+            if (_buttons & (1 << 5))
+                xpad_data.B = 0xFF;
+            if (_buttons & (1 << 6))
+                xpad_data.X = 0xFF;
+            if (_buttons & (1 << 7))
+                xpad_data.Y = 0xFF;
 
             //Analog Sticks
-            xpad_data.leftStickX  = _axis[0];
-            xpad_data.leftStickY  = _axis[1];
+            xpad_data.leftStickX = _axis[0];
+            xpad_data.leftStickY = _axis[1];
             xpad_data.rightStickX = _axis[2];
             xpad_data.rightStickY = _axis[5];
-            xpad_data.L           = _axis[3] >> 2;
-            xpad_data.R           = _axis[4] >> 2;
+            xpad_data.L = _axis[3] >> 2;
+            xpad_data.R = _axis[4] >> 2;
 
             break;
 #if (0)
@@ -246,8 +277,10 @@ void loop()
     }
 
     static uint16_t old_rumble_l, old_rumble_r;
-    if (xid_get_report(&xpad_rumble, sizeof(xpad_rumble)))
-    {
+    //static uint32_t rumble_timeout = 0;
+    if (/*(millis() - rumble_timeout > 1000) && */xid_get_report(&xpad_rumble, sizeof(xpad_rumble)))
+    {   
+        
         bool update_needed = false;
         if (xpad_rumble.lValue != old_rumble_l || xpad_rumble.rValue != old_rumble_r)
         {
@@ -258,18 +291,16 @@ void loop()
         if (update_needed)
         {
             uint8_t l_rumble, r_rumble;
+            l_rumble = max(xpad_rumble.lValue & 0xFF, xpad_rumble.lValue >>8);
+            r_rumble = max(xpad_rumble.rValue & 0xFF, xpad_rumble.rValue >>8);
             //XBONE have rumble values of 0-100;
             if (joy.joystickType() == JoystickController::XBOXONE)
             {
-                l_rumble = (xpad_rumble.lValue & 0xFF) * 100 / 0xFF;
-                r_rumble = (xpad_rumble.rValue & 0xFF) * 100 / 0xFF;
-            }
-            else
-            {
-                l_rumble = (xpad_rumble.lValue & 0xFF);
-                r_rumble = (xpad_rumble.rValue & 0xFF);
+                l_rumble = (uint32_t)l_rumble * 100 / 0xFF;
+                r_rumble = (uint32_t)r_rumble * 100 / 0xFF;
             }
             joy.setRumble(l_rumble, r_rumble, 0x00);
+            //rumble_timeout = millis();
         }
     }
 }
--- src/usbd/xid_duke.c
+++ src/usbd/xid_duke.c
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "tusb.h"
 #include "device/usbd_pvt.h"
-#include "xid.h"
+#include "xid_duke.h"
 #include "printf.h"
 
 CFG_TUSB_MEM_ALIGN uint8_t epin_buf[64];
@@ -10,35 +10,6 @@ uint8_t ep_out, ep_in;
 uint8_t ep_out_size = 0, ep_in_size = 0;
 static USB_XboxGamepad_InReport_t _xpad_data;
 static USB_XboxGamepad_OutReport_t _xpad_rumble_data;
-
-const uint8_t DUKE_HID_DESCRIPTOR_XID[] = {
-    0x10,
-    0x42,
-    0x00, 0x01,
-    0x01,
-    0x02,
-    0x14,
-    0x06,
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
-};
-
-const uint8_t DUKE_HID_CAPABILITIES_IN[] = {
-    0x00,
-    0x14,
-    0xFF,
-    0x00,
-    0xFF,
-    0xFF, 0xFF, 0xFF,
-    0xFF, 0xFF, 0xFF,
-    0xFF, 0xFF, 0xFF,
-    0xFF, 0xFF, 0xFF,
-    0xFF, 0xFF, 0xFF};
-
-const uint8_t DUKE_HID_CAPABILITIES_OUT[] = {
-    0x00,
-    0x06,
-    0xFF, 0xFF, 0xFF, 0xFF
-};
 
 void xid_init(void)
 {
@@ -87,6 +58,13 @@ bool xid_get_report(USB_XboxGamepad_OutReport_t *report, uint16_t len)
         return false;
 
     memcpy(report, &_xpad_rumble_data, len);
+
+    //Queue request on out endpoint
+    //Most games send to control pipe, but atleast THPS2X sends to out pipe!
+    if (tud_ready() && !usbd_edpt_busy(TUD_OPT_RHPORT, ep_out))
+    {
+        TU_ASSERT(usbd_edpt_xfer(TUD_OPT_RHPORT, ep_out, epout_buf, len));
+    }
     return true;
 }
 
@@ -109,7 +87,7 @@ bool xid_send_report(USB_XboxGamepad_InReport_t *report, uint16_t len)
     if (tud_suspended())
         tud_remote_wakeup();
 
-    //Maintain a locale copy of the report
+    //Maintain a local copy of the report
     memcpy(&_xpad_data, report, len);
 
     //Send it to the host
@@ -122,47 +100,62 @@ bool xid_xfer_cb(uint8_t rhport, uint8_t ep_addr, xfer_result_t result, uint32_t
     (void)rhport;
     (void)result;
     (void)xferred_bytes;
-    if (ep_addr == ep_out)
+
+    //Packet received on ep out pipe. Should be a rumble command
+    if (ep_addr == ep_out && xferred_bytes == sizeof(USB_XboxGamepad_OutReport_t))
     {
-        TU_ASSERT(usbd_edpt_xfer(rhport, ep_addr, epout_buf, ep_out_size));
+        memcpy(&_xpad_rumble_data, epout_buf, 6);
     }
     return true;
 }
 
-bool tud_vendor_control_request_cb(uint8_t rhport, tusb_control_request_t const *request)
+bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const *request)
 {
     if (request->bmRequestType == 0xC1 && request->bRequest == 0x06 && request->wValue == 0x4200)
     {
-        TU_LOG2("Sending DUKE_HID_DESCRIPTOR_XID\r\n");
-        tud_control_xfer(rhport, request, (void *)DUKE_HID_DESCRIPTOR_XID, sizeof(DUKE_HID_DESCRIPTOR_XID));
+        if (stage == CONTROL_STAGE_SETUP)
+        {
+            TU_LOG1("Sending DUKE_HID_DESCRIPTOR_XID\r\n");
+            tud_control_xfer(rhport, request, (void *)DUKE_DESC_XID, sizeof(DUKE_DESC_XID));
+        }
     }
     else if (request->bmRequestType == 0xC1 && request->bRequest == 0x01 && request->wValue == 0x0100)
     {
-        TU_LOG2("Sending DUKE_HID_CAPABILITIES_IN\r\n");
-        tud_control_xfer(rhport, request, (void *)DUKE_HID_CAPABILITIES_IN, sizeof(DUKE_HID_CAPABILITIES_IN));
+        if (stage == CONTROL_STAGE_SETUP)
+        {
+            TU_LOG1("Sending DUKE_HID_CAPABILITIES_IN\r\n");
+            tud_control_xfer(rhport, request, (void *)DUKE_CAPABILITIES_IN, sizeof(DUKE_CAPABILITIES_IN));
+        }
     }
     else if (request->bmRequestType == 0xC1 && request->bRequest == 0x01 && request->wValue == 0x0200)
     {
-        TU_LOG2("Sending DUKE_HID_CAPABILITIES_OUT\r\n");
-        tud_control_xfer(rhport, request, (void *)DUKE_HID_CAPABILITIES_OUT, sizeof(DUKE_HID_CAPABILITIES_OUT));
+        if (stage == CONTROL_STAGE_SETUP)
+        {
+            TU_LOG1("Sending DUKE_HID_CAPABILITIES_OUT\r\n");
+            tud_control_xfer(rhport, request, (void *)DUKE_CAPABILITIES_OUT, sizeof(DUKE_CAPABILITIES_OUT));
+        }
     }
     //Get HID Report
     else if (request->bmRequestType == 0xA1 && request->bRequest == 0x01 && request->wValue == 0x0100)
     {
-        tud_control_xfer(rhport, request, (void *)&_xpad_data, sizeof(_xpad_data));
+        if (stage == CONTROL_STAGE_SETUP)
+        {
+            tud_control_xfer(rhport, request, (void *)&_xpad_data, sizeof(_xpad_data));
+        }
     }
     //Set HID Report
     else if (request->bmRequestType == 0x21 && request->bRequest == 0x09 && request->wValue == 0x0200 && request->wLength == 0x06)
     {
-        //Host has sent a rumble command.
-        //Xbox seems to send this to the control pipe most of the time. First try read here.
-        TU_ASSERT(tud_control_xfer(rhport, request, epout_buf, request->wLength));
-
-        //If that fails read from the outpipe. THPS2X is the only game I know that needs this
-        if (epout_buf[1] != 0x06)
-            TU_ASSERT(usbd_edpt_xfer(rhport, 0x00, epout_buf, 6));
-
-        memcpy(&_xpad_rumble_data, epout_buf, sizeof(_xpad_rumble_data));
+        if (stage == CONTROL_STAGE_SETUP)
+        {
+            //Host is sending a rumble command to control pipe. Queue receipt.
+            TU_ASSERT(tud_control_xfer(rhport, request, epout_buf, request->wLength));
+        }
+        else if (stage == CONTROL_STAGE_ACK)
+        {
+            //Receipt complete. Copy data to rumble struct
+            memcpy(&_xpad_rumble_data, epout_buf, sizeof(_xpad_rumble_data));
+        }
     }
     else
     {
@@ -183,6 +176,27 @@ bool tud_vendor_control_complete_cb(uint8_t rhport, tusb_control_request_t const
     return true;
 }
 
+//Invoked when received GET DEVICE DESCRIPTOR
+//Application return pointer to descriptor
+uint8_t const *tud_descriptor_device_cb(void)
+{
+    return (uint8_t const *)&DUKE_DESC_DEVICE;
+}
+
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
+{
+    (void)index;
+    return DUKE_DESC_CONFIGURATION;
+}
+
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+{
+    (void)index;
+    (void)langid;
+
+    return NULL;
+}
+
 static const usbd_class_driver_t const xid_driver =
     {
 #if CFG_TUSB_DEBUG >= 2
@@ -191,14 +205,12 @@ static const usbd_class_driver_t const xid_driver =
         .init = xid_init,
         .reset = xid_reset,
         .open = xid_open,
-        .control_request = tud_vendor_control_request_cb,
-        .control_complete = tud_vendor_control_complete_cb,
+        .control_xfer_cb = tud_vendor_control_xfer_cb,
         .xfer_cb = xid_xfer_cb,
         .sof = NULL};
 
 usbd_class_driver_t const *usbd_app_driver_get_cb(uint8_t *driver_count)
 {
-    printf("usbd_app_driver_get_cb\r\n");
     *driver_count = *driver_count + 1;
     return &xid_driver;
 }
