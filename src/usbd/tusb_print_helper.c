1ca0c180158273e9eb4ddc32393eadd9323bf177
Ryzee119 <wendland@live.com.au>
2020-11-24 11:16:26 +1030

    usbd: Simplify debug printer

--- src/usbd/tusb_print_helper.c
+++ src/usbd/tusb_print_helper.c
@@ -1,10 +1,10 @@
 #include "printf.h"
+#ifdef CFG_TUSB_DEBUG_PRINTF
 int CFG_TUSB_DEBUG_PRINTF(const char *format, ...)
 {
-    char printer[256];
     va_list args;
     va_start(args, format);
-    vsnprintf(printer, sizeof(printer), format, args);
-    printf(printer);
+    vprintf(format, args);
     return 1;
 }
+#endif
