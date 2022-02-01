0adf51e916d691d9850642b48695f12a475a117a
Ryzee119 <wendland@live.com.au>
2022-02-01 15:53:42 +1030

    XMU: Bring up to latest teensyduino

--- src/xmu.cpp
+++ src/xmu.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
+#include <SD.h>
 #include <tusb.h>
-#include <fatfs/ff.h>
 #include "usbd_top.h"
+
+#if MSC_XMU > 0
 
 #ifndef XMU_FILENAME
 #define XMU_FILENAME "XMU_MSC.bin" //13 character limit
@@ -10,57 +12,54 @@
 #define XMU_SIZE_MB 0
 #endif
 
-static FIL xmu_msc_file;
+static FsFile xmu_msc_file;
 static bool sd_ok = false;
+static bool ejected = false;
 static uint32_t MSC_BLOCK_SIZE = 512;
 static uint32_t MSC_BLOCK_NUM = XMU_SIZE_MB * 1024 * 1024 / 512;
 
 void xmu_init()
 {
-    FRESULT res;
-    static const BYTE SZ_TBL = 255;
-    static DWORD clmt[SZ_TBL];
-
-    res = f_open(&xmu_msc_file, XMU_FILENAME, FA_READ | FA_WRITE | FA_CREATE_NEW);
-    if (res == FR_EXIST)
+    if (!SD.sdfs.begin(SdioConfig(FIFO_SDIO)))
     {
-        //File already exists, open it then set the emulated size to match the file size
-        res = f_open(&xmu_msc_file, XMU_FILENAME, FA_READ | FA_WRITE);
-        if (res != FR_OK)
+        TU_LOG1("Error initialising SD card. Is it inserted and formatted correctly?\n");
+        sd_ok = false;
+        return;
+    }
+
+    if (SD.sdfs.exists(XMU_FILENAME))
+    {
+        xmu_msc_file = SD.sdfs.open(XMU_FILENAME, O_RDWR);
+        if (xmu_msc_file == false)
         {
+            TU_LOG1("Error opening %s for WRITE\n");
             sd_ok = false;
             return;
         }
-        TU_LOG1("%s already exists.\n", XMU_FILENAME);
+        MSC_BLOCK_NUM = xmu_msc_file.fileSize() / MSC_BLOCK_SIZE;
+        TU_LOG1("Existing %s opened with %d blocks\n", XMU_FILENAME, MSC_BLOCK_NUM);
     }
     else
     {
-        res = f_lseek(&xmu_msc_file, MSC_BLOCK_NUM * MSC_BLOCK_SIZE);
-        if (res != FR_OK || (f_tell(&xmu_msc_file) != (MSC_BLOCK_NUM * MSC_BLOCK_SIZE)))
+        xmu_msc_file = SD.sdfs.open(XMU_FILENAME, O_RDWR | O_CREAT);
+        if (xmu_msc_file == false)
         {
+            TU_LOG1("Error creating %s\n");
             sd_ok = false;
             return;
         }
+        //Expand the file to XMU_SIZE_MB
+        uint8_t data[MSC_BLOCK_SIZE];
+        for (uint32_t  i = 0; i < MSC_BLOCK_NUM; i++)
+        {
+            xmu_msc_file.write(data, MSC_BLOCK_SIZE);
+        }
+        xmu_msc_file.flush();
     }
-
-    xmu_msc_file.cltbl = clmt;
-    clmt[0] = SZ_TBL;
-    res = f_lseek(&xmu_msc_file, CREATE_LINKMAP);
-    if (res != FR_OK)
-    {
-        sd_ok = false;
-        return;
-    }
-
-    res = f_rewind(&xmu_msc_file);
-    if (res != FR_OK)
-    {
-        sd_ok = false;
-        return;
-    }
-
-    TU_LOG1("%s opened ok! File size: %d MB\n", XMU_FILENAME, f_size(&xmu_msc_file) / 1024);
+    xmu_msc_file.rewind();
     sd_ok = true;
+    ejected = false;
+    return;
 }
 
 void xmu_task(uint8_t type_index)
@@ -70,61 +69,49 @@ void xmu_task(uint8_t type_index)
 extern "C" int32_t tud_msc_read10_cb(uint8_t lun, uint32_t lba, uint32_t offset, void *buffer, uint32_t bufsize)
 {
     (void)lun;
-    FRESULT res;
-    UINT br;
+    uint32_t b;
     TU_LOG2("READ10: lba: %i, off: %i, len: %i...", lba, offset, bufsize);
     if (sd_ok == false)
     {
-        TU_LOG1("ERROR!\n");
         return 0;
     }
-
-    res = f_lseek(&xmu_msc_file, lba * MSC_BLOCK_SIZE + offset);
-    if (res != FR_OK)
+    if (xmu_msc_file.seekSet(lba * MSC_BLOCK_SIZE + offset) == false)
     {
-        TU_LOG1("ERROR!\n");
+        TU_LOG1("%s: SEEK ERROR\n", __FUNCTION__);
         return 0;
     }
 
-    res = f_read(&xmu_msc_file, buffer, bufsize, &br);
-    if (res != FR_OK)
+    b = xmu_msc_file.read(buffer, bufsize);
+    if (b != bufsize)
     {
-        TU_LOG2("ERROR!\n");
-        return 0;
+        TU_LOG1("%s: READ ERROR %d VS %d bytes\n", __FUNCTION__, b, bufsize);
+        return (int32_t)b;
     }
-    TU_LOG2("OK!\n");
-
-    return br;
+    return (int32_t)b;
 }
 
 extern "C" int32_t tud_msc_write10_cb(uint8_t lun, uint32_t lba, uint32_t offset, uint8_t *buffer, uint32_t bufsize)
 {
     (void)lun;
-    UINT bw;
-    FRESULT res;
+    uint32_t b;
     TU_LOG2("WRITE10: lba: %i, off: %i, len: %i...", lba, offset, bufsize);
     if (sd_ok == false)
     {
-        TU_LOG1("ERROR!\n");
         return 0;
     }
-
-    res = f_lseek(&xmu_msc_file, lba * MSC_BLOCK_SIZE + offset);
-    if (res != FR_OK)
+    if (xmu_msc_file.seekSet(lba * MSC_BLOCK_SIZE + offset) == false)
     {
-        TU_LOG1("ERROR!\n");
+        TU_LOG1("%s: SEEK ERROR\n", __FUNCTION__);
         return 0;
     }
 
-    res = f_write(&xmu_msc_file, buffer, bufsize, &bw);
-    if (res != FR_OK)
+    b = xmu_msc_file.write(buffer, bufsize);
+    if (b != bufsize)
     {
-        TU_LOG1("ERROR!\n");
-        return 0;
+        TU_LOG1("%s: WRITE ERROR %d VS %d bytes\n", __FUNCTION__, b, bufsize);
+        return (int32_t)b;
     }
-    TU_LOG2("OK!\n");
-
-    return bw;
+    return (int32_t)b;
 }
 
 extern "C" void tud_msc_capacity_cb(uint8_t lun, uint32_t *block_count, uint16_t *block_size)
@@ -139,7 +126,11 @@ extern "C" void tud_msc_capacity_cb(uint8_t lun, uint32_t *block_count, uint16_t
 extern "C" bool tud_msc_test_unit_ready_cb(uint8_t lun)
 {
     (void)lun;
-    TU_LOG2("tud_msc_test_unit_ready_cb\r\n");
+    if (ejected)
+    {
+        tud_disconnect();
+        sd_ok = false;
+    }
     return sd_ok;
 }
 
@@ -148,8 +139,10 @@ extern "C" bool tud_msc_start_stop_cb(uint8_t lun, uint8_t power_condition, bool
     if (start == 0 && load_eject == 1)
     {
         TU_LOG1("Closing file\n");
-        f_sync(&xmu_msc_file);
-        f_close(&xmu_msc_file);
+        xmu_msc_file.flush();
+        xmu_msc_file.close();
+        ejected = true;
+        tud_msc_set_sense(lun, SCSI_SENSE_NOT_READY, 0x3a, 0x00);
     }
     if (start == 1 && load_eject == 1)
     {
@@ -164,7 +157,7 @@ extern "C" bool tud_msc_start_stop_cb(uint8_t lun, uint8_t power_condition, bool
 extern "C" void tud_msc_inquiry_cb(uint8_t lun, uint8_t vendor_id[8], uint8_t product_id[16], uint8_t product_rev[4])
 {
     (void)lun;
-    TU_LOG2("SCSI_CMD_INQUIRY\r\n");
+    TU_LOG0("SCSI_CMD_INQUIRY\r\n");
 
     const char vid[] = "Ryzee119";
     const char pid[] = "Mass Storage";
@@ -182,3 +175,5 @@ extern "C" int32_t tud_msc_scsi_cb(uint8_t lun, uint8_t const scsi_cmd[16], void
     TU_LOG2("tud_msc_scsi_cb : cmd: %02x\r\n", scsi_cmd[0]);
     return 0;
 }
+
+#endif
